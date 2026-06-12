27311f02fd8376702d725ee8124676b8f6ad0a9d
Harsh  Soni <harshsoni57865@gmail.com>
2026-06-12 18:04:35 +0530

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open('weather-store').then((cache) => {
     return cache.addAll(['index.html', 'weather.js', 'icon.png']);
@@ -6,13 +5,4 @@ self.addEventListener('install', (e) => {
 });
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
-=======
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open('weather-store').then((cache) => {
-    return cache.addAll(['index.html', 'weather.js', 'icon.png']);
-  }));
-});
-self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
->>>>>>> 8809d0bc1b6ad2305e1d6d36299fbeeaade10e8d
 });
